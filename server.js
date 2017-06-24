96fdd5eb44e77123f478a0900aabb67710a74874
Zach Brownson <zbrownson@hotmail.com>
2017-06-23 20:14:45 -0700

    use more real test query

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express')
 var mysql = require('mysql')
 var path = require('path')
 var config = require('./config');
+var cors = require('cors')
+var _ = require('underscore');
 
 const app = express()
 
-app.get('/getStats', function (request, response) {
+app.use(cors())
+
+app.get('/getstats', function (request, response) {
 	
 	var connection = mysql.createConnection({
 	  host: config.mysql.hostname,
@@ -18,17 +22,57 @@ app.get('/getStats', function (request, response) {
 	  if (err) throw err
 	  console.log('You are now connected...')
 	  
-	  connection.query('SELECT * FROM adjusted LIMIT 1', function(err, results) {
-        if (err) throw err
-		var toReturn = "";
-        toReturn += results[0].id + ",";
-        toReturn += results[0].schoolRefName + ",";
-        toReturn += results[0].statName + ",";
-        toReturn += results[0].statValue;
-		response.send(toReturn);
+	  // TODO: let the year be passed into the request
+	  let year = 2016;
+	  
+	  let schoolSql = "select distinct refName from schedules where year = " + year + " order by refName;";
+	  
+	  connection.query(schoolSql, function(error, schools) {
+		if (error) throw error
+		
+			let sql = "select a.* FROM adjusted a LEFT JOIN adjusted b on a.schoolRefName = b.schoolRefName and a.date < b.date WHERE b.date IS NULL  and a.year = " + year + " ORDER BY a.schoolRefName;"
+	  
+			connection.query(sql, function(err, results) {
+				if (err) throw err
+			
+				console.log('here');
+				
+				// build an identity matrix to store school stats
+				let schoolStats = {};
+				_.each(schools, function(school) {
+					schoolStats[school.refName] = {"schoolRefName": school.refName};
+				});	
+
+				console.log(schoolStats);
+				
+				// remove RowDataPacket by transforming to a json string and back into pure json
+				results = JSON.parse(JSON.stringify(results));
+				console.log(results);
+				for (var i = 0; i < results.length; i++) {
+				    schoolStats[results[i].schoolRefName][results[i].statName] = results[i].statValue;
+				}
+				
+				console.log(schoolStats);
+				
+				// transform schoolStats into an array
+				let toReturn = [], item;
+				for (var school in schoolStats) {
+					item = {};
+					item.schoolRefName = school;
+					item.passYards = schoolStats[school].passYards;
+					item.rushYards = schoolStats[school].rushYards;
+					item.passYardsAllowed = schoolStats[school].passYardsAllowed;
+					item.rushYardsAllowed = schoolStats[school].rushYardsAllowed;
+					toReturn.push(item);
+				}
+
+				response.json({items: toReturn});
+		  })		
       })
 	})
 })
+
+
 
 app.get('*', function(request, response) {
 	// load single page app html
